6e146d93890d38397633b10bd856b9f15eadaf1e
Diyora31 <asilovad7>
2024-04-24 12:47:18 +0500

    done

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// let input = prompt('');
+// let str = 'Diyora, Iroda, Bilol';
+// if (str.includes(input)) {
+//     console.log('togri keld');
+// } else {
+//     console.log('togri kelmad');
+// }
+
+// let str1 = 'Web'
+
+// let str2 = 'Brain'
+
+// console.log(str1.replace('Web', 'Brain'));
+// console.log(str2.replace('Brain', 'Web'));
+
+let str = prompt('Enter name')
+let str2 = prompt('enter letter')
+
+if (str.includes(str2)) {
+
+    console.log('this letter is suitable');
+}
+
+else {
+    console.log('this letter is unsuitable');
+}
+
+
+
